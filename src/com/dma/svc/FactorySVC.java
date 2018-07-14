544e3c0004f8616f9bd8f4fdeb5d8b784cc4fc1b
baudelaine <baudelaine@gmail.com>
2018-07-14 09:58:05 +0200

    Cognos Factory is now handling DB2 ORA SQLSRV MYSQL and PGSQL

--- src/com/dma/svc/FactorySVC.java
+++ src/com/dma/svc/FactorySVC.java
@@ -158,6 +158,10 @@ public class FactorySVC {
 				xmlFile = new File(csvc.getPathToXML() + "/DBImport_MYSQL.xml");
 			} else  if (engineName.equals("SQLSRV")) {
 				xmlFile = new File(csvc.getPathToXML() + "/DBImport_SQLSRV.xml");
+			} else  if (engineName.equals("PGSQL")) {
+				xmlFile = new File(csvc.getPathToXML() + "/DBImport_PGSQL.xml");
+			} else  if (engineName.equals("DB2")) {
+				xmlFile = new File(csvc.getPathToXML() + "/DBImport_DB2.xml");
 			}
 
 			SAXReader reader = new SAXReader();
@@ -182,12 +186,18 @@ public class FactorySVC {
 				Element elemSchema = (Element) doc.selectSingleNode("//item/item");
 				elemSchema.addAttribute("Name", schemaName);
 			} else  if (engineName.equals("MYSQL")) {
-				Element elemSchema = (Element) doc.selectSingleNode("//item/item");
-				elemSchema.addAttribute("Name", schemaName);
+				Element elemCatalog = (Element) doc.selectSingleNode("//item/item");
+				elemCatalog.addAttribute("Name", catalogName);
 			} else  if (engineName.equals("SQLSRV")) {
 				Element elemCatalog = (Element) doc.selectSingleNode("//item/item");
 				Element elemSchema = (Element) doc.selectSingleNode("//item/item/item");
 				elemCatalog.addAttribute("Name", catalogName);
+				elemSchema.addAttribute("Name", schemaName);
+			} else  if (engineName.equals("PGSQL")) {
+				Element elemSchema = (Element) doc.selectSingleNode("//item/item");
+				elemSchema.addAttribute("Name", schemaName);
+			} else  if (engineName.equals("DB2")) {
+				Element elemSchema = (Element) doc.selectSingleNode("//item/item");
 				elemSchema.addAttribute("Name", schemaName);
 			}
 			
